63a28391320bb0156dcdbe88e46a76b6dad0a9af
John Hanna <john@behindpixels.com>
2024-01-09 14:28:58 +0400

    Added faq template

---
+++ partials/cta-cards.php
@@ -0,0 +1,61 @@
+<div class="row justify-content-center">
+    <div class="col-lg-3 col-md-6 mb-20">
+        <div class="card bp-card bp-card-rounded h-100 box-shadow p-30">
+            <div class="h-100">
+                <div class="icon location mb-20"></div>
+                <div class="text-strong text-medium mb-10">Visit us</div>
+                <p>
+                    Almas Tower<br>
+                    Jumeirah Lake Towers<br>
+                    Dubai. United Arab Emirates.
+                </p>
+            </div>
+            <div class="col-8">
+            <button class="btn bp-btn btn-primary bp-btn-link mt-20 w-100">Google maps</button>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 mb-20">
+        <div class="card bp-card bp-card-rounded h-100 box-shadow p-30">
+            <div class="h-100">
+                <div class="icon phone-secondary mb-20"></div>
+                <div class="text-strong text-medium mb-10">Talk to us</div>
+                <p>
+                    9:00 am to 9:00 pm<br>
+                    Monday to Friday
+                </p>
+            </div>
+            <div class="col-8">
+                <button class="btn bp-btn btn-primary mt-20 w-100">+971 4 399 9219</button>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 mb-20">
+        <div class="card bp-card bp-card-rounded h-100 box-shadow p-30">
+            <div class="h-100">
+                <div class="icon email mb-20"></div>
+                <div class="text-strong text-medium mb-10">Email us</div>
+                <p>
+                    We are here to help
+                </p>
+            </div>
+            <div class="col-9">
+            <button class="btn bp-btn btn-primary mt-20 w-100">contact@isabullion.com</button>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 mb-20">
+        <div class="card bp-card bp-card-rounded h-100 box-shadow p-30">
+            <div class="h-100">
+                <div class="icon calendar mb-20"></div>
+                <div class="text-strong text-medium mb-10">Meet with us</div>
+                <p>
+                    Meet us virtually
+                </p>
+            </div>
+            <div class="col-8">
+            <button class="btn bp-btn btn-primary mt-20 w-100">Book now</button>
+            </div>
+        </div>
+    </div>
+</div>
